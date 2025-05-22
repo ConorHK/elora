2606db1c8a043ebb2d367aefbf3e58f018a8b80a
Conor Knowles <dev@conorknowles.com>
2025-05-22 18:13:03 +0100

    Add dynamic macro

--- keyboards/splitkb/halcyon/elora/keymaps/default/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/default/rules.mk
@@ -1,5 +1,6 @@
 MOUSEKEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
